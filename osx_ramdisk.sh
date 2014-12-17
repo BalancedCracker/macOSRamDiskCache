3e75f5422fccf32f9d282480ca75c81677ef0f7e
Cole Thompson <cole.thompson@gmail.com>
2014-12-16 21:07:31 -0800

    move more to cache

--- osx_ramdisk.sh
+++ osx_ramdisk.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-TARGET_CACHE_DIRS='Google/Chrome com.apple.Safari com.apple.iTunes'
+LIBRARY_TARGET_CACHE_DIRS='Google/Chrome com.apple.Safari com.apple.iTunes com.spotify.client com.apple.helpd com.apple.dt.Xcode Firefox'
+USER_LIBRARY_CACHE_BASEDIR=$HOME/Library/Caches
 
-USER_CACHE_DIR=$HOME/Library/Caches
-RAM_CACHE_BASEDIR=/Volumes/RamDiskCache
-RAM_CACHE_DIR=$RAM_CACHE_BASEDIR/$USER
+RAM_CACHE_MOUNT_DIR=/Volumes/RamDiskCache
+RAM_CACHE_BASE_DIR=$RAM_CACHE_MOUNT_DIR/$USER
 
 DISK=`/usr/bin/hdiutil attach -nobrowse -nomount ram://2097152`
 /usr/sbin/diskutil erasevolume HFS+ "RamDiskCache" $DISK
+/usr/bin/chflags hidden $RAM_CACHE_MOUNT_DIR
 
 function setup_cache_for {
     TARGET=$1
-    /bin/rm -rvf $USER_CACHE_DIR/$TARGET
-    /bin/mkdir -pv $RAM_CACHE_DIR/$TARGET
-    /bin/ln -v -s $RAM_CACHE_DIR/$TARGET $USER_CACHE_DIR/$TARGET
+    /bin/rm -rvf $USER_LIBRARY_CACHE_BASEDIR/$TARGET
+    /bin/mkdir -pv $RAM_CACHE_BASE_DIR/$TARGET
+    /bin/ln -v -s $RAM_CACHE_BASE_DIR/$TARGET $USER_LIBRARY_CACHE_BASEDIR/$TARGET
 }
    
-for CACHEDIR in $TARGET_CACHE_DIRS; do
+for CACHEDIR in $LIBRARY_TARGET_CACHE_DIRS; do
     setup_cache_for $CACHEDIR
 done
 
-/usr/bin/chflags hidden $RAM_CACHE_BASEDIR
