e8027e0707300315dd04ce6c64b636e57a729054
glaszig <glaszig@gmail.com>
2017-01-02 04:17:52 +0100

    shortening variable names, parameterized volume detection logic

--- ramdisk.sh
+++ ramdisk.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-LIBRARY_TARGET_CACHE_DIRS='Google/Chrome com.apple.Safari com.apple.iTunes com.spotify.client com.apple.helpd com.apple.dt.Xcode Firefox'
-USER_LIBRARY_CACHE_BASEDIR=$HOME/Library/Caches
+CACHE_DIRS='Google/Chrome com.apple.Safari com.apple.iTunes com.spotify.client com.apple.helpd com.apple.dt.Xcode Firefox'
+LIBRARY_CACHES=$HOME/Library/Caches
 
-RAM_CACHE_MOUNT_DIR=/Volumes/RamDiskCache
-RAM_CACHE_BASE_DIR=$RAM_CACHE_MOUNT_DIR/$USER
+RAMDISK_NAME="RamDiskCache"
+MOUNTPOINT="/Volumes/${RAMDISK_NAME}"
+RAMDISK_ROOT="$MOUNTPOINT/$USER"
 
-DISK=$(/usr/bin/hdiutil info|awk '/RamDiskCache/ { print $1 }')
+DISK=$(/usr/bin/hdiutil info|awk "/$RAMDISK_NAME/ { print \$1 }")
 if [ -z "$DISK" ]; then
   DISK=`/usr/bin/hdiutil attach -nobrowse -nomount ram://2097152`
   /usr/sbin/diskutil erasevolume HFS+ "RamDiskCache" $DISK
 fi
-/usr/bin/chflags hidden $RAM_CACHE_MOUNT_DIR
+/usr/bin/chflags hidden $MOUNTPOINT
 
 function setup_cache_for {
-    TARGET=$1
-    /bin/rm -rvf $USER_LIBRARY_CACHE_BASEDIR/$TARGET
-    /bin/mkdir -pv $RAM_CACHE_BASE_DIR/$TARGET
-    /bin/ln -v -s $RAM_CACHE_BASE_DIR/$TARGET $USER_LIBRARY_CACHE_BASEDIR/$TARGET
+    d=$1
+    /bin/rm -rvf $LIBRARY_CACHES/$d
+    /bin/mkdir -pv $RAMDISK_ROOT/$d
+    /bin/ln -v -s $RAMDISK_ROOT/$d $LIBRARY_CACHES/$d
 }
 
-for CACHEDIR in $LIBRARY_TARGET_CACHE_DIRS; do
-    setup_cache_for $CACHEDIR
-done
+for d in $CACHE_DIRS; do setup_cache_for $d; done
